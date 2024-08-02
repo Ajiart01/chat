0bd8df7cd855bce0d6cf5b55a27ab785c2a4d633
Ajiart01 <139549928+Ajiart01@users.noreply.github.com>
2024-08-02 23:40:13 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 const express = require('express');
 const path = require('path');
-
 const app = express();
 const PORT = process.env.PORT || 3000;
 
@@ -12,6 +11,21 @@ app.get('/', (req, res) => {
 
 app.get('/home', (req, res) => {
   res.sendFile(path.join(__dirname, 'public', 'home.html'));
+});
+
+// Menambahkan middleware untuk melakukan redirect
+app.use((req, res, next) => {
+  if (req.path === '/' && req.method === 'GET') {
+    // Jika request adalah GET ke '/', kirim file welcome.html
+    res.sendFile(path.join(__dirname, 'public', 'welcome.html'));
+    
+    // Setelah 5 detik, redirect ke '/home'
+    setTimeout(() => {
+      res.redirect('/home');
+    }, 5000);
+  } else {
+    next();
+  }
 });
 
 app.listen(PORT, () => {
