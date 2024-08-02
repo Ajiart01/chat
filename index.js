f414d03565dae3138406ed641d73e7bd4432c00c
Ajiart01 <139549928+Ajiart01@users.noreply.github.com>
2024-08-02 23:52:41 +0700

    Update index.js

--- index.js
+++ index.js
@@ -5,26 +5,23 @@ const PORT = process.env.PORT || 3000;
 
 app.use(express.static(path.join(__dirname, 'public')));
 
-// Menghapus route handler untuk '/'
+app.get('/', (req, res) => {
+  res.send(`
+    <html>
+      <head>
+        <meta http-equiv="refresh" content="5;url=/home">
+        <title>Welcome</title>
+      </head>
+      <body>
+        <h1>Welcome!</h1>
+        <p>You will be redirected to home page in 5 seconds...</p>
+      </body>
+    </html>
+  `);
+});
 
 app.get('/home', (req, res) => {
   res.sendFile(path.join(__dirname, 'public', 'home.html'));
-});
-
-// Middleware untuk menangani redirect
-app.use((req, res, next) => {
-  if (req.path === '/' && req.method === 'GET') {
-    res.sendFile(path.join(__dirname, 'public', 'welcome.html'), (err) => {
-      if (err) {
-        next(err);
-      } else {
-        // Setelah file terkirim, set header untuk redirect
-        res.set('Refresh', '5; url=/home');
-      }
-    });
-  } else {
-    next();
-  }
 });
 
 app.listen(PORT, () => {
